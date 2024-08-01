55907082bd00d19a178f3fb66cbc96bbd056eeff
tomtomk6 <53164241+tomtomk6@users.noreply.github.com>
2024-08-01 15:35:12 +0200

    cleaned coded and added statistics

--- target.py
+++ target.py
@@ -1,8 +1,35 @@
 import requests
 import json
 import pandas as pd
+import openpyxl
 
-def get_stats(mbox, assetid):
+entry_columns = {
+    "test_id": [],
+    "mbox": [],
+    "assetid": [],
+    "sum_recommended_properties": [],
+    "min_items_per_property": [],
+    "max_items_per_property": [],
+    "mean_items_per_property": [],
+    "median_items_per_property": [],
+    "items_per_assettype": [],
+}
+
+assetids = [
+    "ooyalaID=efaa9cd1-cfea-4e83-8969-d70db6e70c2a",
+    "ooyalaID=ea33eb87-f16c-4f42-83e6-35441c622f07",
+    "ooyalaID=08d3bcd8-6fd8-434e-8fea-71ce721799b6",
+    "ooyalaID=3205403e-418a-4b03-a909-093ab59c7f47",
+    "ooyalaID=42a25328-8eb5-4b84-a67c-c246b1a6a504",
+    "ooyalaID=519ff64a-9df4-4d6c-a58f-29ca35677664",   
+]
+
+mboxes = [
+    "recoDataVideos",
+    "recoData-3371"
+]
+
+def get_df_from_target(mbox, assetid):
     url = "https://rtldisneyfernsehengm.tt.omtrdc.net/rest/v1/delivery?client=rtldisneyfernsehengm&sessionId=d359234570e04f14e1faeeba02d6ab9914e"
     
     params = {
@@ -34,95 +61,102 @@ def get_stats(mbox, assetid):
           "token": "f32451a0-69be-1d1b-e23e-b6130854c9de"
       }
     }
-    
-    response = requests.post(url, json = params)
-    data = json.loads(response.text)
-    
-    
-    columns = {
-    'id': [],
-    'name': [],
-    'assetType': []
-    }
 
-    df = pd.DataFrame(columns)
-
-    string = json.loads(data["execute"]["mboxes"][0]["options"][0]["content"])
-
+    
     columns = {
         'id': [],
-        'name': [],
+        'property': [],
         'assetType': []
     }
 
-    df = pd.DataFrame(columns)
+    df_response = pd.DataFrame(columns)
+   
+    response = requests.post(url, json = params)
+    data = json.loads(response.text)
+    dataslice = json.loads(data["execute"]["mboxes"][0]["options"][0]["content"])
 
-    for key, value in string["adobeRecommendations"]["recDetailedResults"].items():
+    for key, value in dataslice["adobeRecommendations"]["recDetailedResults"].items():
         values = {
             "id": value["id"],
-            "name": value["name"],
+            "property": value["name"],
             "assetType": value["assetType"]
         }
-        value_df = pd.DataFrame([values])
-        df = pd.concat([df, value_df], ignore_index=True)
+        df_values = pd.DataFrame([values])
+        df_response = pd.concat([df_response, df_values], ignore_index=True)
     
-    df["name"] = df["name"].str.split('|').str[0]
-    #number_name = df["name"].value_counts()
-    sum_recommended_properties = number_name = df["name"].nunique()
-        
-    return sum_recommended_properties    
-
-test_columns = {
-    "test_id": [],
-    "mbox": [],
-    "assetid": [],
-    "sum_recommended_properties": []   
-}
+    df_response["property"] = df_response["property"].str.split('|').str[0]
     
-recommended_properties_columns = {
-    "recommended_property": [],
-    "sum_recommendations": []
-}
+    return df_response
 
-recommended_assettypes_columns = {
-    "recommended_assettype": [],
-    "sum_recommendations": []
-}
+def get_items_per_property(df_target):
+    items_per_property = df_target["property"].value_counts()
+    df_items_per_property = pd.DataFrame({"property": items_per_property.index, "items": items_per_property.values})
     
-df_tests = pd.DataFrame(test_columns)
-df_recommended_properties = pd.DataFrame(recommended_properties_columns)
-df_recommended_assettypes = pd.DataFrame(recommended_assettypes_columns)
+    return df_items_per_property
+    
+def get_sum_recommended_properties(df_target):
+    sum_recommended_properties = df_target["property"].nunique()
+    
+    return sum_recommended_properties
 
-assetids = [
-    "ooyalaID=efaa9cd1-cfea-4e83-8969-d70db6e70c2a",
-    "ooyalaID=ea33eb87-f16c-4f42-83e6-35441c622f07",
-    "ooyalaID=08d3bcd8-6fd8-434e-8fea-71ce721799b6",
-    "ooyalaID=3205403e-418a-4b03-a909-093ab59c7f47",
-    "ooyalaID=42a25328-8eb5-4b84-a67c-c246b1a6a504",
-    "ooyalaID=519ff64a-9df4-4d6c-a58f-29ca35677664",   
-]
+def get_min_max_items_per_property(df_items_per_property):
+    result = {
+        "min": df_items_per_property["items"].min(),
+        "max": df_items_per_property["items"].max()
+    }
+    
+    return result
 
-mboxes = [
-    "recoDataVideos",
-    "recoData-3371"
-]
+def get_median_items_per_property(df_items_per_property):
+    return df_items_per_property.median(numeric_only=True).values[0]
+
+def get_mean_items_per_property(df_items_per_property):
+    return df_items_per_property.mean(numeric_only=True).values[0]
+
+def get_items_per_assettype(df_target):
+    items_per_assettype = df_target["assetType"].value_counts()
+    df_items_per_assettype = pd.DataFrame({"assettype": items_per_assettype.index, "items": items_per_assettype.values})
+    df_items_per_assettype["items"] = df_items_per_assettype["items"].astype(str)
+    df_items_per_assettype["combined"] = df_items_per_assettype["assettype"] + "=" + df_items_per_assettype["items"]
+    string = "|".join(df_items_per_assettype["combined"].astype(str))
+    return string
 
 test_id = 1
+
+df_test_result = pd.DataFrame([entry_columns])
 for mbox in mboxes:
     for assetid in assetids:
-        sum_recommended_properties = get_stats(mbox, assetid)
+        df_target = get_df_from_target(mbox, assetid)
+        df_items_per_property = get_items_per_property(df_target)
         
+        sum_recommended_properties = get_sum_recommended_properties(df_target)
+        min_items_per_property = get_min_max_items_per_property(df_items_per_property)["min"]
+        max_items_per_property = get_min_max_items_per_property(df_items_per_property)["max"]
+        mean_items_per_property = get_mean_items_per_property(df_items_per_property)
+        median_items_per_property = get_median_items_per_property(df_items_per_property)
+        items_per_assettype = get_items_per_assettype(df_target)
+        
+                
         entry = {
             "test_id": test_id,
             "mbox": mbox,
             "assetid": assetid,
-            "sum_recommended_properties": sum_recommended_properties
+            "sum_recommended_properties": sum_recommended_properties,
+            "min_items_per_property": min_items_per_property,
+            "max_items_per_property": max_items_per_property,
+            "mean_items_per_property": mean_items_per_property,
+            "median_items_per_property": median_items_per_property,
+            "items_per_assettype": items_per_assettype
         }
         
         df_entry = pd.DataFrame([entry])
-        df_tests = pd.concat([df_tests, df_entry], ignore_index=True)
+        df_test_result = pd.concat([df_test_result, df_entry], ignore_index=True)
         
         test_id += 1
 
-df_tests = df_tests.sort_values(by=["assetid","mbox"])
-print(df_tests)
+
+df_test_result = df_test_result.drop(0)
+df_test_result = df_test_result.sort_values(by=["assetid","mbox"])
+writer = pd.ExcelWriter('output.xlsx')
+df_test_result.to_excel(writer)
+writer.save()
